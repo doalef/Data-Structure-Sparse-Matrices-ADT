247b37c100fcb5b17b32874f7081dd5216b0bebf
Amirali-DESKTOP <amirali.ah77@gmail.com>
2017-10-12 19:13:52 +0330

    First Commit

---
+++ SparceMatrices/SparceMatrices/SparceMatrices.cpp
@@ -0,0 +1,14 @@
+// SparceMatrices.cpp : Defines the entry point for the console application.
+//Amirali Ahmadi
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	system("pause");
+    return 0;
+}
+
